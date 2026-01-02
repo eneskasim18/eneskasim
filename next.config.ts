b29595f1ad824d9941f8b0725bd488af4634c6e6
Enes Kasım <eneskasim_93@hotmail.com>
2026-01-02 22:44:57 +0300

    feat: Add conditional `basePath` configuration for production deployments on GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // GitHub Pages repository name needs to be set as basePath for production
+  basePath: process.env.NODE_ENV === 'production' ? '/eneskasim' : '',
   images: {
     unoptimized: true,
   },
